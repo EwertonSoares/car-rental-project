7eae8be16c556a84d0687ba75276990b1227491d
Ewerton Soares da Silva <ewerton@ciandt.com>
2020-03-03 20:05:24 -0300

    Creating APi to vehicles

--- src/main/java/com/car/rental/project/api/services/VehicleServices.java
+++ src/main/java/com/car/rental/project/api/services/VehicleServices.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.car.rental.project.api.entities.Client;
 import com.car.rental.project.api.entities.Vehicle;
 import com.car.rental.project.api.repositoties.VehicleRepository;
 
@@ -14,14 +16,15 @@ public class VehicleServices {
 
     private static final Logger LOGGER=LoggerFactory.getLogger(ClientServices.class);
     
+    @Autowired
     private VehicleRepository vehicleRepository;
     
 
-	public List<Vehicle> findAllClients() {
-		//LOGGER.info("Buscando lista com todos os veiculos");
+	public List<Vehicle> findAll() {
+		LOGGER.info("Buscando lista com todos os veiculos");
 		return vehicleRepository.findAll();
 }
-
+	
 	public Vehicle saveVehicle(Vehicle vehicle) {
 		// TODO Auto-generated method stub
 		return null;
